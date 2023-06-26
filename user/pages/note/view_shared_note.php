e3efb3ff66ecc46a23d257b1c2fd9509aa41f7a3
Naresh kumar <kumarnaresh03061999@gmail.com>
2023-06-26 21:53:30 +0530

    mininor change on view_shared_note

--- user/pages/note/view_shared_note.php
+++ user/pages/note/view_shared_note.php
@@ -2,9 +2,10 @@
 
 $uid = get_session_user_id();
 $id = $_GET['id'];
+
 $query = "SELECT*FROM notestable LEFT JOIN share_note
 ON notestable.id = share_note.note_id
-WHERE share_note.user_id = '$uid'";
+WHERE share_note.id = '$id'";
 
 $data = mysqli_query($connection, $query);
 $result = mysqli_num_rows($data);
