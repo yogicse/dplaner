deb3b91cc56befa60125eb97de307a785cf74034
aditya <aditya.word3090@gmail.com>
2023-07-06 22:08:50 +0530

    Updated file of css

--- user/layouts/footer.php
+++ user/layouts/footer.php
@@ -1,20 +1,34 @@
-         <footer>
+        <footer>
             <div class="a">
                 <div class="col-lg-12">
-                    <p style="color:white;">Copyright &copy; Lab Managed Services <script>document.write(new Date().getFullYear())</script> </p>
+                    <p style="color:#3c72c9;">Copyright &copy; Developed By Aditya <script>document.write(new Date().getFullYear())</script> </p>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
             <!-- /.row -->
         </footer>
     <!-- jQuery -->
+        </div>
 
-    <script type="text/javascript" src="/assets/back/js/custom/common.js"></script>
+
+    
+        <script src="/assets/back/js/jquery.min.js"></script>
+        <script src="/assets/back/js/bootstrap.bundle.min.js"></script>
+            <!-- Core plugin JavaScript-->
+    <script src="/assets/back/js/jquery.easing.min.js"></script>
+        <!-- Custom scripts for all pages-->
+        <script src="/assets/back/js/sb-admin-2.min.js"></script>
+        <script src="/assets/back/datatables/jquery.dataTables.min.js"></script>
+    <script src="/assets/back/datatables/dataTables.bootstrap4.min.js"></script>
+    <!-- <script type="text/javascript" src="/assets/back/js/custom/common.js"></script> -->
 
     <script>
         $("#selectAllBoxes").click(function(){
             $('input:checkbox').not(this).prop('checked', this.checked);
         });
-    </script>
+    </script>    
+
+
+    <script src="/assets/back/datatables/datatables-demo.js"></script>
 </body>
 </html>
--- user/layouts/header.php
+++ user/layouts/header.php
@@ -1,11 +1,11 @@
 <?php
-
-include_once '../includes/db.php';
-
 ob_start();
 if (!isset($_SESSION)) {
     session_start();
 }
+include_once '../includes/db.php';
+
+
 if (empty($_SESSION['role'])) {
     header("Location: /login.php");
 }
@@ -21,56 +21,24 @@ if (empty($_SESSION['role'])) {
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title><?=(isset($page_title) && !empty($page_title)) ? $page_title." - " : ""?> Legal CMS Administration</title>
-
-    <link href="/assets/back/kendo/js/kendo.common.min.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/back/kendo/js/kendo.blueopal.min.css" rel="stylesheet" type="text/css" />
-
-    <link href="/assets/back/kendo/js/kendo.ext.min.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/back/kendo/js/BridgeCRM.min.css" rel="stylesheet" type="text/css" />
-
-
+    <title><?=(isset($page_title) && !empty($page_title)) ? $page_title." - " : ""?>Time Sheet</title>
+    <link href="/assets/back/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+    <link href="/assets/back/css/sb-admin-2.min.css" rel="stylesheet" type="text/css">
     <link href="/assets/back/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="/assets/back/css/sb-admin.css" rel="stylesheet" type="text/css">
     <link href="/assets/back/css/style.css" rel="stylesheet" type="text/css">
     <link href="/assets/back/css/plugins/morris.css" rel="stylesheet" type="text/css">
     <link href="/assets/back/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script type="text/javascript" src="/assets/back/js/jquery.min.js"></script>
     <script type="text/javascript" src="/assets/back/js/bootstrap.min.js"></script>
 
     <script type="text/javascript" src="/assets/ckeditor/ckeditor.js"></script>
     <script type="text/javascript" src="/assets/ckeditor/samples/js/readonlyeditor.js"></script>
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-
-
-    <style>
-        .bulk_options {
-            margin-bottom: 20px;
-            margin-left: 1px;
-        }
-
-        .enable_color {
-            color: forestgreen;
-        }
-
-        .disable_color {
-            color: red;
-        }
-
-        .archive_color {
-            color: gray;
-        }        
-    </style>
-
+    <link href="/assets/back/css/dataTables.bootstrap4.css" rel="stylesheet" type="text/css">
+   
 
 </head>
 
-<body>
+<body id="page-top">
